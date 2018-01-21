06d67c4e456d98d7ae75541327e05b4d8faa951f
Jim Gumbley <jgumbley@thoughtworks.com>
2018-01-21 19:33:57 +0000

    add hints on impact and likelihood

--- src/clj/card_attempt/threats.clj
+++ src/clj/card_attempt/threats.clj
@@ -11,7 +11,7 @@
    [:meta {:name    "viewport"
            :content "width=device-width, initial-scale=1"}]
    (include-css (if (env :dev) "/css/site.css" "/css/site.min.css"))
-   (include-css "https://fonts.googleapis.com/css?family=Open+Sans")
+   (include-css "https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,700,800")
    ])
 
 ;-----------------------------------------------------
@@ -38,7 +38,7 @@
              (threat. "A regulator investigates an infringement of the General Data Protection Regulation"
                       (likelihood. (list
                                     "Do you process the personal data of EU residents?"
-                                    "Is it likely that someone may raise a complaint to the regulator about your organisation?"
+                                    "Might an EU resident raise a complaint to a regulator about your organisation?"
                                     "Is it likely that you will be subject to a data breach?"))
                       ""),
              (threat. "An end user or third party tries to commit fraud by providing false information"
@@ -46,7 +46,7 @@
                                     "Is it possible for someone to gain financially by using your system?"
                                     "Could someone cause harm or mischief to a third party by using your system?"))
                       ""),
-             (threat. "A disgruntled or angry employee or ex-exployee tries to embarrass or hurt the organisation"
+             (threat. "A disgruntled employee or ex-exployee tries to embarrass or hurt the organisation"
                       (likelihood. (list
                                     "Are there signs of poor morale? Are managers aware of concerns of this type?"
                                     "If the organisation is large, might we not know if there was a threat?"
@@ -57,26 +57,35 @@
                       (likelihood. (list
                                     "Does the system connect to other networks?"))
                       ""),
-             (threat. "A random botnet or scriptkiddy mounts a simple automated attack on the system"
+             (threat. "A developer or admin makes an error in configuring or securing the system"
+                      (likelihood. (list
+                                    "Does the system connect to the Internet?"))
+                      ""),
+             (threat. "A random botnet or scriptkiddy mounts an automated attack on the system"
                       (likelihood. (list
                                     "Does the system connect to the Internet?"))
                       ""),
              (threat. "An internal user tries to access sensitive information out of curiosity"
                       (likelihood. (list
                                     "Does the system have a large number of end users?"
-                                    "Does the system store any sensitive data?"))
+                                    "Does the system store sensitive data that people might find of interest?"))
                       "")])
 
 (defn draw-threat [card]
   [:div.bigcard
-   [:div.card-threat-main "THREAT"]
-   [:div.card-threat-title (:title card)]
-   [:div.card-threat-subtitle "LIKELIHOOD"]
-   [:ul
-    (for [bullet (:bullets (:likelihood card))] [:li bullet])
+   [:div.bigcard-header
+      [:div.card-threat-main "THREAT"]
+      [:div.card-threat-title (:title card)]
     ]
-   [:div.card-threat-subtitle "IMPACT"]
-   [:div.card-threat-title (:impact card)]
+   [:div.bigcard-column
+      [:div.card-threat-subtitle "LIKELIHOOD"]
+      [:ul (for [bullet (:bullets (:likelihood card))] [:li bullet]) ]
+    ]
+   [:div.bigcard-column
+      [:div.card-threat-subtitle "IMPACT"]
+      [:ul (for [bullet (:bullets (:impact card))] [:li bullet]) ]
+    ]
+   [:div.card-category "Sensible Conversations about Security"]
    ]
   )
 
