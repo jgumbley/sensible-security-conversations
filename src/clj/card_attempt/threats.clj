83288ebcaab6ba9db7e940e2f1d66e1e9b2566a6
Jim Gumbley <jgumbley@thoughtworks.com>
2018-01-21 16:05:11 +0000

    adding some more detail on threats

--- src/clj/card_attempt/threats.clj
+++ src/clj/card_attempt/threats.clj
@@ -16,21 +16,67 @@
 
 ;-----------------------------------------------------
 
-(defrecord threat [title])
+(defrecord threat [title likelihood impact])
+(defrecord likelihood [bullets])
+(defrecord impact [bullets])
 
-(def threats
-  [
-   (threat. "A cyber criminal group, hacktivist or nation state tries to steal data from the system"),
-   (threat. "A cyber criminal or hacktivist group mounts a denial of service attack on the system"),
-   (threat. "A regulator issues a penalty under the General Data Protection Regulation"),
-   (threat. "An end user or third party commits fraud by providing false information"),
-   (threat. "A disgruntled or angry employee or ex-exployee abuses access to embarrass or hurt the organisation"),
-   (threat. "A random botnet or scriptkiddy mounts a simple automated attack on the system"),
-   (threat. "An internal user casually accesses information they are not entited to out of curiosity")])
+(def threats[
+             (threat. "A cyber criminal, hacktivist or nation state group tries to steal data from the system"
+                      (likelihood. (list
+                                   "What value might data in the system have to an attacker?"
+                                   "Does the data have exchange value? Can it be sold?"
+                                   "Could an attacker demand a ransom if they obtained the data?"
+                                   "Are there specific reasons an attacker might target the data in your system?"
+                                   "Can the data be used to escalate privledge into other more sensitive systems?"))
+                      ""),
+             (threat. "A cyber criminal or hacktivist group mounts a denial of service attack on the system"
+                      (likelihood. (list
+                                    "Could an attacker demand a ransom were your system unavailable?"
+                                    "Is it in the interest of any group to impact your reputation by taking your system down?"
+                                    "Are there knock on effect on third parties or clients which an attack could benefit from?"))
+                      ""),
+             (threat. "A regulator investigates an infringement of the General Data Protection Regulation"
+                      (likelihood. (list
+                                    "Do you process the personal data of EU residents?"
+                                    "Is it likely that someone may raise a complaint to the regulator about your organisation?"
+                                    "Is it likely that you will be subject to a data breach?"))
+                      ""),
+             (threat. "An end user or third party tries to commit fraud by providing false information"
+                      (likelihood. (list
+                                    "Is it possible for someone to gain financially by using your system?"
+                                    "Could someone cause harm or mischief to a third party by using your system?"))
+                      ""),
+             (threat. "A disgruntled or angry employee or ex-exployee tries to embarrass or hurt the organisation"
+                      (likelihood. (list
+                                    "Are there signs of poor morale? Are managers aware of concerns of this type?"
+                                    "If the organisation is large, might we not know if there was a threat?"
+                                    "Is it possible that someone could be laid off under a dispute?"
+                                    "Are there previous examples of this kind of threat?"))
+                      ""),
+             (threat. "A new form of ransomware or a wiper worm tries to spread via adjacent networks"
+                      (likelihood. (list
+                                    "Does the system connect to other networks?"))
+                      ""),
+             (threat. "A random botnet or scriptkiddy mounts a simple automated attack on the system"
+                      (likelihood. (list
+                                    "Does the system connect to the Internet?"))
+                      ""),
+             (threat. "An internal user tries to access sensitive information out of curiosity"
+                      (likelihood. (list
+                                    "Does the system have a large number of end users?"
+                                    "Does the system store any sensitive data?"))
+                      "")])
 
 (defn draw-threat [card]
   [:div.bigcard
-   [:div.card-category (:title card)]
+   [:div.card-threat-main "THREAT"]
+   [:div.card-threat-title (:title card)]
+   [:div.card-threat-subtitle "LIKELIHOOD"]
+   [:ul
+    (for [bullet (:bullets (:likelihood card))] [:li bullet])
+    ]
+   [:div.card-threat-subtitle "IMPACT"]
+   [:div.card-threat-title (:impact card)]
    ]
   )
 
