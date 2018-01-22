810ac509a2f8613ac112f85a8a400d952d9eb1c3
Jim Gumbley <jgumbley@thoughtworks.com>
2018-01-22 11:56:20 +0000

    add wording for impact questions

--- src/clj/card_attempt/threats.clj
+++ src/clj/card_attempt/threats.clj
@@ -23,76 +23,97 @@
 (def threats[
              (threat. "A cyber criminal, hacktivist or nation state group try to steal data from the system"
                       (likelihood. (list
-                                   "What value might data in the system have to an attacker?"
-                                   "Does the data have exchange value? Can it be sold?"
-                                   "Could an attacker demand a ransom if they obtained the data?"
-                                   "Are there specific reasons an attacker might target the data in your system?"
-                                   "Can the data be used to escalate privledge into other systems?"))
+                                    "What value might data in the system have to an attacker?"
+                                    "Does the data have exchange value? Can it be sold?"
+                                    "Could an attacker demand a ransom if they obtained the data?"
+                                    "Are there specific reasons an attacker might target the data in your system?"
+                                    "Can the data be used to escalate privledge into other systems?"))
                       (impact.     (list
-                                   "An impact on reputation with customers, revenue, trustworthiness of brand?"
-                                   "Impact on trust with parent organisation?"
-                                   "May trigger an investigation and penalties by regulator"
-                                   "Possible financial cost in ransoms"
-                                   "Impact if data used to escalate privledge on more sensitive systems?"))),
+                                    "How might data loss impact the organisation's reputation?"
+                                    "Might there be a breakdown in trust with any parent organisation or customers?"
+                                    "Might this trigger an investigation or penalties by a regulator?"
+                                    "Might there be a financial cost in terms of revenue? Ransoms?"
+                                    "What impact if data allows escalation of privledge to more sensitive systems?"))),
              (threat. "A cyber criminal or hacktivist group mount a denial of service attack on the system"
                       (likelihood. (list
-                                   "Could an attacker demand a ransom were your system unavailable?"
-                                   "Is it in the interest of any group to impact your reputation by taking your system down?"
-                                   "Are there knock on effect on third parties or clients which an attack could benefit from?"))
+                                    "Could an attacker demand a ransom were your system unavailable?"
+                                    "Is it in the interest of any group to impact your reputation by taking your system down?"
+                                    "Are there knock on effect on third parties or clients which an attack could benefit from?"))
                       (impact.     (list
-                                   "What is the impact on your org?"))),
+                                    "What is the impact on revenue or operations if the system is down?"
+                                    "How long could the system be down until it really hurt? 5 mins? 1 hour? 1 week?"
+                                    ))),
              (threat. "A regulator investigates an infringement of the General Data Protection Regulation"
                       (likelihood. (list
                                     "Do you process the personal data of EU residents?"
                                     "Might an EU resident raise a complaint to a regulator about your organisation?"
                                     "Is it likely that you will be subject to a data breach?"))
                       (impact.     (list
-                                    "What is the impact on your org?"))),
+                                     "Fines for failing to report a data breach can be up to 2% yearly revenue"
+                                     "Fines for non compliance with GDPR can be up to 4% of global yearly revenue"
+                                     "Penalties in most cases will be public record, leading to a reputational impact"))),
              (threat. "An end user or third party tries to commit fraud by providing false information"
                       (likelihood. (list
-                                    "Is it possible for someone to gain financially by using your system?"
+                                    "Are there previous examples of this kind of threat for this or similar systems?"
+                                    "Could someone to gain financially by making an application or other action using your system?"
                                     "Could someone cause harm or mischief to a third party by using your system?"))
                       (impact.     (list
-                                    "What is the impact on your org?"))),
+                                    "What is the impact of a single fraudulent transaction?"
+                                    "How does this change if there are means of replaying or multiplying the fraud?"))),
              (threat. "A disgruntled employee or ex-exployee tries to embarrass or hurt the organisation"
                       (likelihood. (list
                                     "Are there signs of poor morale? Are managers aware of concerns of this type?"
-                                    "If the organisation is large, might we not know if there was a threat?"
                                     "Is it possible that someone could be laid off under a dispute?"
+                                    "If something like this is detected, there will be legal implications. Would someone take the risk?"
                                     "Are there previous examples of this kind of threat?"))
                       (impact.     (list
-                                    "What is the impact on your org?"))),
+                                    "What impact could someone with a normal level of access to the system make?"
+                                    "What impact could someone with high levels of technical access to system make?"))),
              (threat. "A new form of ransomware or a wiper worm tries to spread from adjacent networks"
                       (likelihood. (list
                                     "Does the system connect to other networks?"))
                       (impact.     (list
-                                    "What is the impact on your org?"))),
+                                    "What would be the impact if you had to rebuild your environment from scratch?"
+                                    "Would there be dataloss if the environment was destroyed?"
+                                    "How long would the system be unavailable for in the event of a rebuild?"))),
              (threat. "A developer or admin makes an error in configuring or securing the system"
                       (likelihood. (list
-                                    "Does the system connect to the Internet?"))
+                                    "Human error can never be ruled out, although you can use automation and review to reduce issues."))
                       (impact.     (list
-                                    "What is the impact on your org?"))),
+                                    "What might be the impact of misconfiguring access control?"
+                                    "What might be the impact of a misconfiguration of transport encryption?"))),
              (threat. "A random botnet or scriptkiddy mounts an automated attack on the system"
                       (likelihood. (list
-                                    "Does the system connect to the Internet?"))
+                                    "Should be considered likely if the system connects to the Internet"))
                       (impact.     (list
-                                    "What is the impact on your org?"))),
+                                    "What is the business impact of spam comments, applications or signups?"
+                                    "What is the effect of a system component being taken offline?"
+                                    "What is the impact of a user account being brute-forced and access sold?"
+                                    "What is the rebuild / forensic cost if a machine is compromised?"))),
              (threat. "An internal user tries to access sensitive information out of curiosity"
                       (likelihood. (list
                                     "Does the system have a large number of end users?"
                                     "Does the system store sensitive data that people might find of interest?"))
                       (impact.     (list
-                                    "What is the impact on your org?"))),
+                                    "What is the administrative cost of a data breach?"
+                                    "What is the reputational impact if personal nformation leaks to press?"
+                                    "What is the privacy impact on the individual?"
+                                    "Could there be regulatory impact?"))),
              (threat. " "
                       (likelihood. (list
-                                    "What is the likelihood of this threat occuring?"))
+                                    " "))
                       (impact.     (list
-                                    "What could the impact be on your organisation?"))),
+                                    " "))),
              (threat. " "
                       (likelihood. (list
-                                    "What is the likelihood of this threat occuring?"))
+                                    " "))
                       (impact.     (list
-                                    "What could the impact be on your organisation?"))),
+                                    " "))),
+             (threat. " "
+                      (likelihood. (list
+                                    " "))
+                      (impact.     (list
+                                    " "))),
                       ])
 
 (defn draw-threat [card]
